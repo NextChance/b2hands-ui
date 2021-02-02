0f310adad87a87b8c18a5d256223a15e0d5fde91
Hugo Hernandez <hugo.hernandez@nc43tech.com>
2021-02-02 13:19:22 +0000

    Add storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,27 @@
+const path = require('path');
+const rootPath = path.resolve(__dirname, '../src')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-viewport', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            additionalData: `@import "@/assets/scss/_config.scss";`
+          }
+        }
+      ],
+    })
+
+    config.resolve.alias['@'] = rootPath
+    config.resolve.alias['~'] = rootPath
+
+    return config;
+  }
+};
