50a584ddf7739df1318f4ce44f57863ab28589b2
Hugo Hernandez <hugo.hernandez@nc43tech.com>
2021-02-18 15:36:25 +0000

    feat: create modal component

---
+++ src/components/ui-modal.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    v-if="value"
+    class="modal"
+  >
+    <div class='modal__header wrapper--s'>
+      <i class="modal__icon b2i-modal" @click="handleClose"></i>
+    </div>
+    <div class="modal__content">
+      <!-- @slot Default slot for modal content -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'UiModal',
+  props: {
+    /**
+     * Value that shows or hides the modal
+     */
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    /**
+     * Close modal
+     */
+    handleClose(): void {
+      /**
+       * Close event
+       *
+       * @event on-modal-close
+       */
+      this.$emit('on-modal-close')
+    }
+  }
+})
+</script>
+
+<style lang='scss' scoped>
+.modal {
+  background-color: $white;
+  color: $black-100;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 999;
+
+  &__icon {
+    cursor: pointer;
+    left: $spacing-size-4;
+    position: absolute;
+    top: $spacing-size-4;
+    &:before {
+      content: "\e90d";
+    }
+  }
+
+  &__content {
+    box-sizing: border-box;
+    padding-left: $spacing-size-8;
+    padding-top: $spacing-size-3;
+  }
+
+  @media (min-width: $breakpoint-s) {
+    &__header {
+      display: flex;
+      justify-content: flex-end;
+      box-sizing: border-box;
+      margin-top: $spacing-size-6;
+      margin-bottom: $spacing-size-12;
+    }
+
+    &__content {
+      padding-top: unset;
+      padding-left: unset;
+    }
+
+    &__icon {
+      left: unset;
+      position: static;
+      top: unset;
+      &:before {
+        content: "\e909";
+      }
+    }
+  }
+}
+</style>
