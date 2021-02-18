9574fe4ae9ad96a71b2279a859aa8e99ee29581c
Hugo Hernandez <hugo.hernandez@nc43tech.com>
2021-02-18 15:36:41 +0000

    feat: create modal story

---
+++ src/stories/UiModal/UiModal.stories.js
@@ -0,0 +1,36 @@
+import { action } from '@storybook/addon-actions'
+import UiModal from '~/components/ui-modal'
+
+export default {
+  title: 'UiModal',
+  component: UiModal
+}
+
+export const carouselWithSeveralImages = () => ({
+  components: { UiModal },
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  template: `
+    <div>
+        <button @click='toogleOpen'>modal</button>
+        <ui-modal v-model='isOpen' @on-modal-close='closeModal'>
+          <div class='wrapper'>
+            <span>Content</span>
+          </div>
+        </ui-modal>
+    </div>
+  `,
+  methods: {
+    toogleOpen() {
+      action('open modal')
+      this.isOpen = !this.isOpen
+    },
+    closeModal() {
+      action('event to close modal')
+      this.isOpen = false
+    }
+  }
+})
