7165f13b5dcb661b7e11abcdf98345b0591efe16
IamLoWLo <iamlowlo@gmail.com>
2021-04-28 16:19:35 +0200

    feat(LazyInvent): add loading animation

--- src/tools/errorImage.ts
+++ src/tools/errorImage.ts
@@ -20,11 +20,51 @@ export const replaceNodeWithErrorImage = (nodeElement: HTMLElement) => {
   nodeElement.parentElement?.replaceChild(errorImage, nodeElement)
 }
 
+export const generateLoaderImage = () => {
+    const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs')
+    const gradient = document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient')
+    gradient.setAttribute('id', 'loaderGradient')
+    gradient.setAttribute('x1', '0')
+    gradient.setAttribute('x2', '0')
+    gradient.setAttribute('y1', '0')
+    gradient.setAttribute('y2', '1')
+    const stop1 = document.createElementNS('http://www.w3.org/2000/svg', 'stop')
+    stop1.setAttribute('offset', '0%')
+    stop1.setAttribute('stop-color', 'rgb(247, 247, 247)')
+    stop1.setAttribute('stop-opacity', '0')
+    const stop2 = document.createElementNS('http://www.w3.org/2000/svg', 'stop')
+    stop2.setAttribute('offset', '50%')
+    stop2.setAttribute('stop-color', 'white')
+    stop2.setAttribute('stop-opacity', '.5')
+    const stop3 = document.createElementNS('http://www.w3.org/2000/svg', 'stop')
+    stop3.setAttribute('offset', '100%')
+    stop3.setAttribute('stop-color', 'rgb(247, 247, 247)')
+    stop3.setAttribute('stop-opacity', '0')
+    gradient.appendChild(stop1)
+    gradient.appendChild(stop2)
+    gradient.appendChild(stop3)
+    defs.appendChild(gradient)
+    const loader = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+    loader.setAttribute('x', '0')
+    loader.setAttribute('y', '0')
+    loader.setAttribute('height', '100%')
+    loader.setAttribute('width', '100%')
+    loader.setAttribute('class', 'placeholder-image__loader')
+    loader.setAttribute('fill', 'url(#loaderGradient)')
+    return {
+        defs,
+        loader
+    }
+}
+
 export const addSiblingNodeWithLoadingImage = (nodeElement: HTMLElement) => {
-  const loadingImage: HTMLElement = document.createElement('div')
-  loadingImage.classList.add('placeholder-image', 'placeholder-image--loading')
-  const svg = createPlaceholderImage()
-  loadingImage.appendChild(svg)
-  nodeElement.parentElement?.appendChild(loadingImage)
-  return loadingImage
+    const loadingImage: HTMLElement = document.createElement('div')
+    loadingImage.classList.add('placeholder-image', 'placeholder-image--loading')
+    const svg = createPlaceholderImage()
+    const { defs, loader } = generateLoaderImage()
+    svg.appendChild(defs)
+    svg.appendChild(loader)
+    loadingImage.appendChild(svg)
+    nodeElement.parentElement?.appendChild(loadingImage)
+    return loadingImage
 }
