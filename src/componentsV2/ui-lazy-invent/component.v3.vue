49fcb8cd64be233cde89d00a9757147c29599628
hugohernandez <hugo.hernandez@nc43tech.com>
2021-11-25 14:04:18 +0000

    feat(): refactor ui-lazy-invent

---
+++ src/componentsV2/ui-lazy-invent/component.v3.vue
@@ -0,0 +1,127 @@
+<template src="./index.html"></template>
+<style lang="scss" scoped src="./styles.scss"></style>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted} from 'vue3'
+import { addSiblingNodeWithLoadingImage, replaceNodeWithErrorImage } from '@/tools/errorImage'
+
+export default defineComponent({
+  name: 'ui-lazy-lazy-image',
+  props: {
+    /**
+     * Src url to use in case srset is not available
+     */
+    src: {
+      type: String,
+      default: ''
+    },
+    /**
+     * Formatted srcset
+     */
+    srcset: {
+      type: String,
+      default: ''
+    },
+    /**
+     * Usage image size
+     */
+    sizes: {
+      type: String,
+      default: ''
+    },
+    /**
+     * Text to use in accesibility mode
+     */
+    alt: {
+      type: String,
+      default: ''
+    },
+    /**
+     * Whether the image should still showing a loading state until parent requires it
+     */
+    delayLoad: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Whether the image should still showing a loading state until parent requires it
+     */
+    isErrorForced: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Predefined image height to show while loading
+     */
+    loadingHeight: {
+      type: Number,
+      default: 0
+    },
+    /**
+     * Predefined image width to show while loading
+     */
+    loadingWidth: {
+      type: Number,
+      default: 0
+    },
+    forceImageLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: [
+    'image-visible',
+    'on-image-error',
+    'on-image-loaded'
+  ],
+  setup (props, {emit}) {
+    const visibilityPlaceholder = ref(null)
+    const main = ref(null)
+
+    let isImageLoaded = false
+    let isHidden = true
+    let loadingImage:any = null
+
+    const rand = (Math.random() * 1000).toFixed()
+
+    const onVisibilityChanged = (isVisible: boolean): void => {
+      if (isVisible) {
+        isHidden = false
+        loadingImage = addSiblingNodeWithLoadingImage(
+          visibilityPlaceholder as HTMLElement
+        )
+        loadingImage?.classList.add('lazy-image__loading')
+        if (props.loadingHeight && props.loadingWidth) {
+          loadingImage.style.height = `${props.loadingHeight}px`
+          loadingImage.style.width = `${props.loadingWidth}px`
+        }
+        (visibilityPlaceholder as HTMLElement).classList.add('lazy-image--hide')
+        emit('image-visible')
+      }
+    }
+
+    const onImageLoaded = (): void => {
+      loadingImage?.classList.add('lazy-image__loading--loaded')
+      isImageLoaded = true
+      setTimeout(() => {
+        loadingImage?.remove()
+      }, 350)
+      emit('on-image-loaded', main)
+    }
+
+    const onImageError = (evt: Event): void => {
+      replaceNodeWithErrorImage(evt.currentTarget as HTMLElement)
+      loadingImage?.remove()
+      emit('on-image-error')
+    }
+
+    onMounted(() => {
+      if (props.isErrorForced) {
+        replaceNodeWithErrorImage(visibilityPlaceholder as HTMLElement)
+      }
+    })
+
+    return { isImageLoaded, isHidden, rand, onImageError, onImageLoaded, onVisibilityChanged }
+  }
+})
+</script>
