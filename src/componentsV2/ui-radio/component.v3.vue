34fa27d9b69e53e01979cbd7738073fc229e2a2b
hugohernandez <hugo.hernandez@nc43tech.com>
2021-11-25 14:10:05 +0000

    feat(): refactor ui-radio

---
+++ src/componentsV2/ui-radio/component.v3.vue
@@ -0,0 +1,61 @@
+<template src="./index.html"></template>
+<style lang="scss" scoped src="./styles.scss"></style>
+
+<script lang="ts">
+import { defineComponent} from 'vue3'
+
+export default defineComponent({
+  name: '',
+  props: {
+    /**
+     * Sets input name
+     */
+    name: {
+      type: String,
+      default: ''
+    },
+    /**
+     * Sets input value
+     */
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    /**
+     * Sets input checked status
+     */
+    isChecked: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Sets input checked status
+     */
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Text to show beside radio
+     */
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: [
+    'change'
+  ],
+  setup (props, { emit }) {
+
+    const onChange = (value: string|number): void => {
+      if (!props.isDisabled) {
+        emit('change', value)
+      }
+    }
+    return {
+      onChange
+    }
+  }
+})
+</script>
