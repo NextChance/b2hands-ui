1df9abae11068a66382d8419eaec2449e12c3b6d
IamLoWLo <iamlowlo@gmail.com>
2021-11-29 10:41:36 +0100

    feat(UiComponents): add refactored UiTabs

---
+++ src/componentsV2/ui-tabs/component.v3.vue
@@ -0,0 +1,32 @@
+<template src="./index.html"></template>
+<style lang="scss" scoped src="./styles.scss"></style>
+
+<script lang="ts">
+import { defineComponent} from 'vue3'
+
+export default defineComponent({
+  name: 'UiTabs',
+  props: {
+    tabs: {
+      type: Array,
+      default: () => ([])
+    },
+    activeTabId: {
+      type: String,
+      default: ''
+    },
+    hasScroll: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props, { emit }) {
+    const handleTabClick = (tab: string) => {
+      emit('on-tab-clicked', tab)
+    }
+    return {
+      handleTabClick
+    }
+  }
+})
+</script>
