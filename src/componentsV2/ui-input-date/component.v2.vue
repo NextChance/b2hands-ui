010f6884affbc791ee72bb4bf179ee92a1338435
Virginia <virginia.blesa@gmail.com>
2021-11-29 14:51:00 +0100

    core/migrate components to vue3: - ui-input-date - ui-modal - ui-profile-header

---
+++ src/componentsV2/ui-input-date/component.v2.vue
@@ -0,0 +1,74 @@
+<template src="./index.html"></template>
+<style scoped lang="scss" src="./style.scss"></style>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'UiInputDate',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    isReadonly: {
+      type: Boolean,
+      default: false
+    },
+    max: {
+      type: String,
+      default: ''
+    },
+    min: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: Number,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      selectedDate: ''
+    }
+  },
+  computed: {
+    formattedDate (): string {
+      return this.selectedDate !== ''
+        ? this.formatDate(this.selectedDate)
+        : this.placeholder
+    }
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler (value): void {
+        if (value) {
+          this.selectedDate = new Date(value).toISOString().replace(/T.*/,'')
+        }
+      }
+    }
+  },
+  methods: {
+    formatDate (date: string): string {
+      const d = new Date(date)
+      return d.toLocaleString(navigator.language || 'es', {
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric'
+      })
+    },
+
+    handleChangeDate (): void {
+      this.$emit('change', new Date(this.selectedDate).getTime())
+    }
+  }
+})
+</script>
---
+++ src/componentsV2/ui-modal/component.v2.vue
@@ -0,0 +1,32 @@
+<template src="./index.html"></template>
+<style scoped lang="scss" src="./styles.scss"></style>
+
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'UiModal',
+  props: {
+    /**
+     * Value that shows or hides the modal
+     */
+    isOpen: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    /**
+     * Close modal
+     */
+    handleClose(): void {
+      /**
+       * Close event
+       *
+       * @event on-modal-close
+       */
+      this.$emit('on-modal-close')
+    }
+  }
+})
+</script>
+
