ba24148e2f77810dc25eafca96301b13efc045ce
Virginia <virginia.blesa@gmail.com>
2022-02-04 15:04:24 +0100

    feat/CHAR-377 - Add display for one image carousel https://nextchance.atlassian.net/browse/CHAR-377 https://www.figma.com/file/xBmaPKuX0k7tpYwOg3JWOd/NoFakes-%2F-Product-Definition-0.0.1?node-id=819%3A62188

--- src/fixtures/ui-carousel.fixture.js
+++ src/fixtures/ui-carousel.fixture.js
@@ -95,7 +95,23 @@ export const itemsMock2 = [
 export const itemsMockErr = [
   {
     src:
-    'https://previews.123rf.com/images/andreaobzerova/andreaobzerova1405/andreaobzerova140500048/28447933-imagen-vertical-del-color-de-ramas-de-%C3%A1rboles-de-cerezo-en-flor-procesamiento-posterior-suave-de-la-ve.jpg',
+      'https://www.jardineriaon.com/wp-content/uploads/2018/04/flores-decorativas-y-viosas.jpg',
+    alt: 'image5'
+  }
+]
+
+export const itemsMockOneHorizontal = [
+  {
+    src:
+      'https://image.freepik.com/foto-gratis/banner-vista-panoramica-hermosas-flores-rosas-floreciendo_100787-2367.jpg',
     alt: 'image3'
+  }
+]
+
+export const ItemsMockOne = [
+  {
+    src:
+      'https://www.jardineriaon.com/wp-content/uploads/2018/04/flores-decorativas-y-vistosas.jpg.webp',
+    alt: 'image4'
   }
 ]
