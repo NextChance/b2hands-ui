26176f3cf274db9a7eab5c8d6888149ff8d05c1b
Virginia <virginia.blesa@gmail.com>
2022-02-18 14:09:52 +0100

    Feat/CHAR-733 - Create ui-stars component to use in rating
    Related with: https://nextchance.atlassian.net/browse/CHAR-733

---
+++ src/componentsV2/ui-stars/component.v2.vue
@@ -0,0 +1,31 @@
+<template src="./index.html"></template>
+<style lang="scss" scoped src="./styles.scss"></style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'UiStars',
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    isEditable: {
+      type: Boolean,
+      default: false
+    },
+    totalAmount: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onClick(item: number): void {
+      if (this.isEditable) {
+        const total = item === this.value ? item - 1 : item
+        this.$emit('on-rating-selected', total)
+      }
+    }
+  }
+})
+</script>
