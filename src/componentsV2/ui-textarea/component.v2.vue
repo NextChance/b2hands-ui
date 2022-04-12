ebb1ca0e770a7649b7dac83e81b0503ea1210f54
IamLoWLo <iamlowlo@gmail.com>
2022-04-12 11:47:53 +0200

    feat(Component/UiTextarea): refactor to use class component on V2

--- src/componentsV2/ui-textarea/component.v2.vue
+++ src/componentsV2/ui-textarea/component.v2.vue
@@ -3,98 +3,65 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  name: 'UiTextarea',
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    id: {
-      type: String,
-      default: 'uitextarea'
-    },
-    maxLength: {
-      type: Number,
-      default: 1000
-    },
-    name: {
-      type: String,
-      default: ''
-    },
-    size: {
-      type: Number,
-      default: 35
-    },
-    value: {
-      type: String,
-      default: ''
-    },
-    placeholder: {
-      type: String,
-      default: ''
-    }
-  },
-  data() {
-    return {
-      inputValue: '',
-      textAreaHeight: ''
-    }
-  },
+import { Component, Prop, Watch } from 'vue-property-decorator'
 
-  watch: {
-    value(): void {
-      this.inputValue = this.value
-    }
-  },
+@Component
+export default class UiTextarea extends Vue {
+  inputValue: String = ''
+  textAreaHeight: String = '0px'
 
-  created() {
-    if (this.value) this.inputValue = this.value
-  },
+  @Prop({ default: false }) disabled!: boolean
+  @Prop({ default: 'uitextarea' }) id!: String
+  @Prop({ default: 1000 }) maxLength!: Number
+  @Prop({ default: '' }) name!: String
+  @Prop({ default: 35 }) size!: Number
+  @Prop({ default: '' }) value!: String
+  @Prop({ default: '' }) placeholder!: String
+  @Prop({ default: true }) hasAutofocus!: boolean
+
+
+  @Watch('value', { immediate: true })
+  onValueChange (newValue: string) {
+    this.inputValue = newValue
+  }
 
   mounted() {
-    const uiTextarea = this.$refs.textarea as HTMLInputElement
     this.handleAutoResize()
-    uiTextarea.blur()
-    if (uiTextarea.value !== '') {
-      this.focusInput()
-    }
-  },
-
-  methods: {
-    focusInput(): void {
+    if (this.hasAutofocus) {
       const uiTextarea = this.$refs.textarea as HTMLInputElement
-      uiTextarea.focus()
-    },
-
-    handleFocus(): void {
-      this.$emit('input-focus-event')
-    },
-
-    handleBlur(ev: Event): void {
-      this.$emit('input-blur-event', ev)
-    },
-
-    handleInput(): void {
-      const uiTextarea = this.$refs.textarea as HTMLInputElement
-      this.handleAutoResize()
-      this.$emit('input', uiTextarea.value)
-    },
-
-    handleKeyUp(ev: Event): void {
-      this.$emit('input-key-up-event', ev)
-    },
-
-    handleAutoResize(): void {
-      const uiTextarea = this.$refs.textarea as HTMLInputElement
-      this.textAreaHeight = '0px'
-      uiTextarea.blur()
-      this.$nextTick(function () {
-        const container = this.$refs.container as HTMLInputElement
-        this.textAreaHeight = (uiTextarea.scrollHeight > container.offsetHeight ? uiTextarea.scrollHeight : container.offsetHeight) + 'px'
+      if (uiTextarea.value !== '') {
         uiTextarea.focus()
-      })
+      }
     }
   }
-})
+
+  handleFocus(): void {
+    this.$emit('input-focus-event')
+  }
+
+  handleBlur(ev: Event): void {
+    this.$emit('input-blur-event', ev)
+  }
+
+  handleInput(): void {
+    const uiTextarea = this.$refs.textarea as HTMLInputElement
+    this.handleAutoResize()
+    this.$emit('input', uiTextarea.value)
+  }
+
+  handleKeyUp(ev: Event): void {
+    this.$emit('input-key-up-event', ev)
+  }
+
+  handleAutoResize(): void {
+    const uiTextarea = this.$refs.textarea as HTMLInputElement
+    this.textAreaHeight = '0px'
+    this.$nextTick(function () {
+      const container = this.$refs.container as HTMLInputElement
+      this.textAreaHeight = (uiTextarea.scrollHeight > container.offsetHeight
+        ? uiTextarea.scrollHeight
+        : container.offsetHeight) + 'px'
+    })
+  }
+}
 </script>
